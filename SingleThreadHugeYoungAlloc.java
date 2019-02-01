b40727b196a62e1103a49394fe7cdec3364a9d9f
Maruthi <maruthi.janardhan@gteamstaff.com>
2019-02-01 12:01:15 +0530

    no message

---
+++ SingleThreadHugeYoungAlloc.java
@@ -0,0 +1,24 @@
+package com.mydomain;
+
+import java.net.URL;
+
+/**
+ * Single thread app allocates a lot of young objects with almost nothing going to old gen
+ */
+public class SingleThreadHugeYoungAlloc {
+
+    public static void main(String args[]) throws Exception {
+        long start = System.currentTimeMillis();
+        int k = 0;
+        for (int i = 0; i < 100000000; i++) {
+            String temp = new String("Str" + i);
+            k += temp.length();
+        }
+        for (int i = 0; i < 100000000; i++) {
+            URL u = new URL("http://something.co" + i);
+            k += u.getPort();
+        }
+        System.out.println("Duration : " + (System.currentTimeMillis() - start));
+        Thread.sleep(300000);
+    }
+}
