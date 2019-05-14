837ce0fd85472b4e5dbd1e8ab00050799539d118
Maruthi <maruthi.janardhan@gteamstaff.com>
2019-05-14 10:14:53 +0530

    no message

---
+++ SingleThreadBuildingOldAlloc.java
@@ -0,0 +1,25 @@
+package mydomain;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Single thread app allocates a lot of young objects with almost nothing going to old gen
+ */
+public class SingleThreadBuildingOldAlloc {
+
+    public static void main(String args[]) throws Exception {
+        long start = System.currentTimeMillis();
+        List<String> strList = new ArrayList<>();
+        List<URL> urlList = new ArrayList<>();
+        for (int i = 0; i < 100000000; i++) {
+            strList.add("Str" + i);
+        }
+        for (int i = 0; i < 100000000; i++) {
+            URL u = new URL("http://something.co" + i);
+            urlList.add(u);
+        }
+        System.out.println("Duration : " + (System.currentTimeMillis() - start));
+    }
+}
